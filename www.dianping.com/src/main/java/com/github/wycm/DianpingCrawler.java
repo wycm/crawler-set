745f4cc9c22c3682acfb6403af7eb6e08759d86d
王洋 <wangyang1@u51.com>
2018-11-18 12:37:30 +0800

    update

--- www.dianping.com/src/main/java/com/github/wycm/DianpingCrawler.java
+++ www.dianping.com/src/main/java/com/github/wycm/DianpingCrawler.java
@@ -11,13 +11,17 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * 美团点评字体反爬
+ * xb -> x = 0 - 18 * (columnNumber - 1), y = -7 - 30 * (lineNumber - 1)
+ * ov -> x = -8 - 14 * (columnNumber - 1), y = -7 - 30 * (lineNumber - 1)
  */
 public class DianpingCrawler {
     public static void main(String[] args) throws IOException {
@@ -26,7 +30,7 @@ public class DianpingCrawler {
         String cssUrl = "http:" + document.select("link[href*=svgtextcss]").first().attr("href");
         String cssResponse = Jsoup.connect(cssUrl).execute().body();
 //        System.out.println(document.toString());
-//        System.out.println(cssResponse);
+        System.out.println(cssResponse);
         Pattern pattern = Pattern.compile("class\\^=\"(.*?)\".*?url\\((.*?)\\)");
         Matcher matcher = pattern.matcher(cssResponse);
         Map<String, String> urlMap = new HashMap<>();
@@ -39,5 +43,22 @@ public class DianpingCrawler {
             System.out.println(prefix);
             System.out.println(url);
         }
+        pattern = Pattern.compile("\\.[a-z]{2}-.*?\\{.*?\\}");
+        matcher = pattern.matcher(cssResponse);
+        List<String> cssList = new ArrayList<>();
+        while (matcher.find()){
+//            System.out.println(matcher.group(0));
+            cssList.add(matcher.group(0));
+        }
+//        pattern = Pattern.compile("\\.[a-z]{2}-.*?\\{background:(.*?)\\.0px");
+        cssList.sort((c1, c2) -> {
+            int i = c1.substring(0, 3).compareTo(c2.substring(0, 3));
+            if (i == 0){
+                return c1.substring(8, 28).compareTo(c2.substring(8, 28));
+            } else {
+                return i;
+            }
+        });
+        cssList.forEach(System.out::println);
     }
 }
